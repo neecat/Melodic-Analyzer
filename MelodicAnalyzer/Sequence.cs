5d70d5ed8af50925e7b07cc763691fadf463d5ad
neecat <n.karahalis@gmail.com>
2013-07-30 07:38:07 -0400

    Created a Sequence Class, it doesn't work yet. Once it does, it'll make porting the program to a GUI app much simpler.

---
+++ MelodicAnalyzer/Sequence.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+
+namespace MelodicAnalyzer
+{
+	public class Sequence
+	{
+		//Class Variables
+		//modification: addition of float vars - April 9th, 2013 - 10:18:00 EDT
+		//Associate Letters and Sharps to floating decimal numberss
+
+		//modification: translated to C# and moved into a class - July 30, 2013 - 06:51:00 EDT
+		string[] natnoteletters = { "C", "D", "E", "F", "G", "A", "B" };
+		float[] natural={1.0f,2.0f,3.0f,3.5f,4.5f,5.5f,6.5f};
+
+		string[] sharpnoteletters={"B#","C#","D#","E#","F#","G#","A#"};
+		float[] sharp={1.0f,1.5f,2.5f,3.5f,4.0f,5.0f,6.0f};
+
+		string[] flatnoteletters={"Db","Eb","Fb","Gb","Ab","Bb","Cb"};
+		float[] flat={1.5f,2.5f,3.5f,4.0f,5.0f,6.0f,6.5f};
+
+		int notestotal=0;
+		int currentnote=0;
+		string[] userinput;
+		float[] durations;
+		public Sequence (int numberofnotes)
+		{
+			notestotal = numberofnotes;
+		}
+
+		public void addNote(string note,float duration)
+		{
+			userinput [currentnote] = note;
+			durations [currentnote] = duration;
+			currentnote++;
+		}
+		public float[] analyze(string[] sequ)
+		{
+			float[] output;
+			for (int x=0; x<sequ.Length; x++) {
+				for (int n=0; n<natural.Length; n++) {
+					try{
+						if (sequ [x] == sharpnoteletters [x]) {
+							output [x] = sharp [x];
+							break;
+						}
+						if (sequ [x] == flatnoteletters [x]) {
+							output [x] = flat [x];
+							break;
+						}
+						if (sequ [x] == natnoteletters [x]) {
+							output [x] = natural [x];
+							break;
+						}
+					}catch(IndexOutOfRangeException e) {
+						Console.Write ("Error: Out of range!\n" + e.Message);
+					}
+				}
+			}
+			 return output;
+		}
+	}
+}
+
